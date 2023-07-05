bb1808f5c26709f642ba88a51fe8f70a8ee49fec
Petr <evilsephy1@mail.ru>
2023-07-05 12:23:33 +0300

    Slider
    added 'tiny-slider'

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const slider = tns({
+	container: '.carousel__inner',
+	items: 1,
+	slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: false,
+	speed: 600
+   });
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
