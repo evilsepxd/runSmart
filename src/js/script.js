7898f82eda7dcdb3da02c4a02e16476b7cc05b1d
Petr <evilsephy1@mail.ru>
2023-07-07 11:22:54 +0300

    Modal
    added modal windows & adapted

--- src/js/script.js
+++ src/js/script.js
@@ -36,5 +36,21 @@ $(document).ready(function() {
 
 		toggleSlide('.catalog-item__link');
 		toggleSlide('.catalog-item__back');
+
+		// Modal
+
+		$('[data-modal=consultation]').on('click', function() {
+			$('.overlay, #consultation').fadeIn();
+		});
+		$('.modal__close').on('click', function() {
+			$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+		});
+
+		$('.button_mini').each(function(i) {
+			$(this).on('click', function() {
+				$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+				$('.overlay, #order').fadeIn();
+			});
+		});
 	});
 });
